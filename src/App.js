3f399b1105a07bac928d26f53423399a0518f379
xinghengwang <xing@moesif.com>
2023-05-26 14:20:51 -0700

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import './css/bootstrap-theme.css';
+import './css/bootstrap.css';
+import EmailTemplates from './EmailTemplates';
 
+
+const initialMergeTags = {
+  first_name: {
+    name: "First Name",
+    value: "{{first_name}}"
+  },
+  last_name: {
+    name: "Last Name",
+    value: "{{last_name}}"
+  }
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmailTemplates initialMergeTags={initialMergeTags} />
     </div>
   );
 }
---
+++ src/EmailTemplates.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import EmailEditor from "react-email-editor";
+
+import Button from "react-bootstrap/lib/Button";
+import Modal from "react-bootstrap/lib/Modal";
+
+const STARTING_DESIGN_JSON = { body: { rows: [], values: {} } };
+
+function AddDynamicMergeTagModal(props) {
+  const { onHide, onFinish } = props;
+  return (
+    <Modal show onHide={onHide}>
+      <Modal.Body>Add Dynamic Merge tag.</Modal.Body>
+      <Modal.Footer>
+        <Button
+          onClick={() => {
+            const newTags = {
+              NEW_TAG: {
+                label: "NEW_TAG",
+                value: "{{NEW_TAG}}"
+              }
+            };
+            onFinish(newTags);
+            onHide();
+          }}
+        >
+          Add
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default class EmailTemplates extends Component {
+  constructor(props) {
+    super(props);
+    this.editorRef = React.createRef();
+    this.handleSave = this.handleSave.bind(this);
+    this.handleInitEditor = this.handleInitEditor.bind(this);
+    this.handleToggleModal = this.handleToggleModal.bind(this);
+    this.handleUpdateDynamicTags = this.handleUpdateDynamicTags.bind(this);
+    this.state = {
+      dynamicTags: {},
+      showModal: false
+    };
+  }
+
+  handleSave() {}
+
+  handleToggleModal() {
+    this.setState((prev) => ({
+      ...prev,
+      showModal: !prev.showModal
+    }));
+  }
+
+  handleUpdateDynamicTags(newTags) {
+    const { initialMergeTags } = this.props;
+    this.setState((prev) => ({
+      ...prev,
+      dynamicTags: newTags
+    }));
+
+    const newMergeTags = { ...initialMergeTags, ...newTags };
+    this.editorRef.current.editor.setMergeTags(newMergeTags);
+  }
+
+  handleInitEditor() {
+    // must be delayed a few seconds
+    // for this.editorRef to be ref hooked up
+
+    const { initialMergeTags } = this.props;
+    const { dynamicFields } = this.state;
+    if (this.editorRef.current.editor) {
+      this.editorRef.current.editor.registerCallback("image", (file, done) => {
+        const data = new FormData();
+        data.append("file", file.attachments[0]);
+        // for now just place holder image.
+        done({
+          url: "https://placehold.co/600x400"
+        });
+        // uploadImage({
+        //   orgId,
+        //   appId,
+        //   portalBaseUrl,
+        //   token,
+        //   data,
+        // })
+        //   .then((result) => {
+        //     done({
+        //       url: result && result.url,
+        //     });
+        //   })
+        //   .catch((err) => {
+        //     alert('An error uploading file: ' + err);
+        //   });
+      });
+      this.editorRef.current.editor.loadDesign(STARTING_DESIGN_JSON);
+      const combinedMergeTags = { ...initialMergeTags, ...dynamicFields };
+      this.editorRef.current.editor.setMergeTags(combinedMergeTags);
+    }
+  }
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div
+        style={{
+          width: "800px"
+        }}
+      >
+        <Button onClick={this.handleToggleModal}>
+          Show Add Dynamic Merge Tag Modal
+        </Button>
+
+        <hr />
+        <form>
+          <EmailEditor
+            projectId={4684}
+            displayMode="email"
+            minHeight={800}
+            ref={this.editorRef}
+            onReady={this.handleInitEditor}
+            options={{
+              user: null,
+              safeHtml: true
+            }}
+          />
+        </form>
+        {showModal && (
+          <AddDynamicMergeTagModal
+            onHide={this.handleToggleModal}
+            onFinish={this.handleUpdateDynamicTags}
+          />
+        )}
+      </div>
+    );
+  }
+}
